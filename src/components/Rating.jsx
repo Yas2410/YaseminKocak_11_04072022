911594bfb1443cc0b10569a925fe2dcc410a6e88
Yasemin Kocak <yasemin.kocak@lapiscine.pro>
2022-07-16 16:37:57 +0200

    Rating Component Update

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 //npm install react-icons --save
 //icons react // import d'images dans le dossier
+
+//Images plutot que librarie icons react
 import { VscStarFull } from "react-icons/vsc";
 
+//Données RATING de mon fichier de data
 const Rating = ({ rating }) => {
   const stars = [1, 2, 3, 4, 5];
 
   return (
     <div className="rating">
       {stars.map((star) =>
+        //Si le rating (data.js) est >= star
+        //Alors les étoiles sont rouges
+        //Sinon elles sont grises
         rating >= star ? (
           <VscStarFull
             style={{ color: "#FF6060" }}
